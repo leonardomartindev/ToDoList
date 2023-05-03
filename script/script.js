c0ed8f17b1fac6f0c62c8dae9cf743052c57cc52
leonardomartindev <leonardomartin.dev@gmail.com>
2023-05-03 20:59:45 -0300

    ajust

--- script/script.js
+++ script/script.js
@@ -8,14 +8,34 @@ settingIcon.addEventListener("click", () => {
 })
 
 // Function add task
-const form = document.querySelector("form")
+const form = document.querySelector("form");
 form.addEventListener("submit", (e) =>{
-    e.preventDefault()
-})
+    e.preventDefault();
+});
+
 const input = document.querySelector("#Add-task-input");
 const ul = document.getElementsByClassName("tasks-ul")[0];
 
 function createTask(value){
+    if (!value) {
+        return;
+      }
+    const tasks = ul.querySelectorAll("li");
+    for (let i = 0; i < tasks.length; i++) {
+      const taskLabel = tasks[i].querySelector("label");
+      if (taskLabel.innerHTML === value) {
+        return;
+      }
+    }
+
+    const deleteButton = document.createElement("button");
+    deleteButton.innerHTML = '<i class="fas fa-times"></i>';
+    deleteButton.classList.add("delete-task");
+    deleteButton.addEventListener("click", () => {
+      li.remove();
+    });
+    
+
     const li = document.createElement("li");
     const checkbox = document.createElement("input");
     checkbox.type = "checkbox";
@@ -25,12 +45,14 @@ function createTask(value){
     li.appendChild(checkbox);
     li.appendChild(label);
     li.classList.add("tasks-li");
+    li.appendChild(deleteButton)
     ul.appendChild(li);
 }
 
 input.addEventListener("keydown", (event) =>{
     if (event.keyCode === 13) {
         createTask(input.value); 
+        input.value = "";
       } else{
         return;
       }
